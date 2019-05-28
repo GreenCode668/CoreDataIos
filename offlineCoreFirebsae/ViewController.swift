c65b5b538310b465073597bab89d4d8f3a61e806
saadpasta <saadpasta70@gmail.comm>
2019-05-28 11:36:15 +0500

    Initial Commit

---
+++ offlineCoreFirebsae/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  offlineCoreFirebsae
+//
+//  Created by saad pasta on 28/05/2019.
+//  Copyright © 2019 saad pasta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
